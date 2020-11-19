65dc1364236ec48c4177c2f2b32fd53da2162524
jw00ds <woods.johnc@yahoo.com>
2020-11-19 17:03:50 -0600

    first commit

---
+++ app.py
@@ -0,0 +1,68 @@
+# character_name = "John"
+# character_age = "35"
+# is_male = True
+#
+# print("John is\n35 years old")
+# print(character_name + " is " + character_age + " years old")
+#
+# phrase = "Trail Boss"
+# print(phrase)
+# print(phrase + " is cool")
+# print(phrase.lower())
+# print(phrase.isupper())
+# print(len(phrase))
+# print(phrase[2])
+# print(phrase.index("a"))
+# print(phrase.replace("a", "Z"))
+# phrase2 = phrase.replace("a", "Z")
+# print(phrase2)
+# print(-2.0987)
+#
+# print(3 * 4 + 8)
+# print(3 * (4 + 8))
+# print(10 % 3)
+# my_num = -5
+# print(my_num)
+# print(phrase + " " + str(my_num))
+# print(abs(my_num))
+# print(pow(5, 3))
+# print(max(5, 2))
+# print(min(5, 2))
+# print(round(4.5))
+#
+# from math import *
+# print(ceil(3.7))
+# print(floor(3.7))
+# print(sqrt(36))
+#
+# name = input("Enter your name: ")
+# age = input("Enter your age: ")
+# print("Hello " + name + "! You are " + age)
+
+# num1 = input("Enter a number: ")
+# num2 = input("Enter another number: ")
+# result = float(num1) + int(num2)
+#
+# print(result)
+#
+# color1 = "Red"
+# color2 = "Blue"
+#
+# print("Roses are red")
+# print("violets are blue")
+# print(f'{color1} roses')
+# print(f'violets are {color2.lower()}')
+
+friends = ["Josh", "Craig"]
+friends2 = ["James", "Pete", "Cam"]
+
+print(friends)
+print(friends[1])
+print(friends[-1])
+print(friends[0:2])
+friends.extend(friends2)
+print(friends)
+friends.append("Jake")
+print(friends)
+
+
